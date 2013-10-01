f345bee0f8979a69ab8f358ab6b1c65de7132142
mabarroso <mabarroso@mabarroso.com>
2013-10-01 14:23:39 +0200

    Set php-curl and Google API

--- vagrant_provision_sync.sh
+++ vagrant_provision_sync.sh
@@ -2,7 +2,7 @@
 sudo apt-get update >/dev/null 2>&1
 
 echo "Installing essentials..."
-    apt-get install -y bash curl openssl git subversion php5-cli >/dev/null 2>&1
+    apt-get install -y bash curl openssl git subversion php5-cli php5-curl >/dev/null 2>&1
 
 echo "Tuning sync..."
     su - -c '/bin/bash --login /vagrant/vagrant_provision_tuning_sync.sh' vagrant
--- vagrant_provision_tuning_sync.sh
+++ vagrant_provision_tuning_sync.sh
@@ -5,5 +5,5 @@ cd /vagrant/sync
 curl -sS https://getcomposer.org/installer | php
 php composer.phar --quiet install
 
-svn checkout --quiet http://google-api-php-client.googlecode.com/svn/trunk/ /tmp/google-api-php-client
-mv /tmp/google-api-php-client ./vendor/google-api-php-client
+svn checkout --quiet http://google-api-php-client.googlecode.com/svn/trunk/ /tmp/google-api
+mv /tmp/google-api ./vendor/google-api
