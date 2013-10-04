72f8db11fa74b2f337596e3ebab4309c1544c256
mabarroso <mabarroso@mabarroso.com>
2013-10-04 09:22:55 +0200

    Vendor refactoring

--- vagrant_provision_tuning_sync.sh
+++ vagrant_provision_tuning_sync.sh
@@ -4,6 +4,7 @@ cd /vagrant/sync
 
 curl -sS https://getcomposer.org/installer | php
 php composer.phar --quiet install
+ln -s /vagrant/Sync/vendor/bartosz-maciaszek/php-rtm/src/Rtm vendor/Rtm
 
 svn checkout --quiet http://google-api-php-client.googlecode.com/svn/trunk/ /tmp/google-api
 mv /tmp/google-api ./vendor/google-api
